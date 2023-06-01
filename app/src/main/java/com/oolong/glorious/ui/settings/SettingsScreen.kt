90394766e90d82d533bb6896d0ab134e822323c8
Christopher Goldman <dev@oolong.com>
2023-06-01 14:51:54 -0700

    feat: Add Settings screen

---
+++ app/src/main/java/com/oolong/glorious/ui/settings/SettingsScreen.kt
@@ -0,0 +1,85 @@
+package com.oolong.glorious.ui.settings
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.produceState
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.Lifecycle.State.STARTED
+import androidx.lifecycle.repeatOnLifecycle
+import com.oolong.glorious.ui.theme.GloriousTheme
+import androidx.compose.material3.ExperimentalMaterial3Api
+import com.oolong.glorious.ui.UiState
+
+@Composable
+fun SettingsScreen(modifier: Modifier = Modifier, viewModel: SettingsViewModel = hiltViewModel()) {
+    val lifecycle = LocalLifecycleOwner.current.lifecycle
+    val uiState by produceState<UiState<String>>(
+        initialValue = UiState.Loading,
+        key1 = lifecycle,
+        key2 = viewModel
+    ) {
+        lifecycle.repeatOnLifecycle(state = STARTED) {
+            viewModel.uiState.collect { value = it }
+        }
+    }
+    if (uiState is UiState.Success) {
+        SettingsScreen(
+            text = (uiState as UiState.Success).data,
+            onSave = {},
+            modifier = modifier
+        )
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+internal fun SettingsScreen(
+    text: String,
+    onSave: (name: String) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(modifier) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 24.dp),
+            horizontalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            Text(text = text)
+        }
+    }
+}
+
+// Previews
+
+@Preview(showBackground = true)
+@Composable
+private fun DefaultPreview() {
+    GloriousTheme {
+        SettingsScreen("Kotlin", onSave = {})
+    }
+}
+
+@Preview(showBackground = true, widthDp = 480)
+@Composable
+private fun PortraitPreview() {
+    GloriousTheme {
+        SettingsScreen("Kotlin", onSave = {})
+    }
+}
