d24201f804036b74e5a5b20c8065e7716386fb39
Christopher Goldman <dev@oolong.com>
2023-06-01 14:54:05 -0700

    feat: Define Settings page for nav, wrap nav in a Scaffold

--- app/src/main/java/com/oolong/glorious/ui/Navigation.kt
+++ app/src/main/java/com/oolong/glorious/ui/Navigation.kt
@@ -1,36 +1,43 @@
-/*
- * Copyright (C) 2022 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.oolong.glorious.ui
 
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
+import androidx.navigation.NavDestination
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.oolong.glorious.ui.message.MessageScreen
+import com.oolong.glorious.ui.navigation.Screen
+import com.oolong.glorious.ui.settings.SettingsScreen
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainNavigation() {
-    val navController = rememberNavController()
-
-    NavHost(navController = navController, startDestination = "main") {
-        composable("main") { MessageScreen(modifier = Modifier.padding(16.dp)) }
-        // TODO: Add more destinations
+  val navController = rememberNavController()
+  val navBackStackEntry by navController.currentBackStackEntryAsState()
+  val currentDestination = navBackStackEntry?.destination
+  val displayTitle = (Screen forName currentDestination?.route)?.displayName ?: Screen.MAIN.displayName
+  Scaffold(
+    topBar = {
+      TopAppBar(
+        title = {
+          Text(text = displayTitle)
+        }
+      )
     }
+  ) { paddingValues ->
+    val modifier = Modifier.padding(paddingValues)
+    NavHost(navController = navController, startDestination = Screen.MAIN.screenName) {
+      composable(Screen.MAIN.screenName) { MessageScreen(navController, modifier) }
+      composable(Screen.SETTINGS.screenName) { SettingsScreen(modifier) }
+    }
+  }
 }
