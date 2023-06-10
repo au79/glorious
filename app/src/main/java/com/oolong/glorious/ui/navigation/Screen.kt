478ce872c6f9357449f9e68deed96dab9e1a1b5c
Christopher Goldman <dev@oolong.com>
2023-06-10 00:07:46 -0700

    feat: Rework Screen enum to add name, icon, and description values

--- app/src/main/java/com/oolong/glorious/ui/navigation/Screen.kt
+++ app/src/main/java/com/oolong/glorious/ui/navigation/Screen.kt
@@ -1,10 +1,24 @@
 package com.oolong.glorious.ui.navigation
 
-enum class Screen(val screenName: String, val displayName: String) {
-  MAIN("main", "Glorious"),
-  SETTINGS("settings", "Settings");
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import com.oolong.glorious.R
+
+enum class Screen(
+  val route: String,
+  @StringRes val displayNameRes: Int,
+  @DrawableRes val iconRes: Int?,
+  @StringRes val iconDescriptionRes: Int?,
+) {
+  MAIN("main", R.string.app_name, null, null),
+  SETTINGS(
+    "settings",
+    R.string.settings_screen_display_name,
+    R.drawable.settings_icon,
+    R.string.settings_icon_description
+  );
 
   companion object {
-    infix fun forName(value: String?): Screen? = Screen.values().firstOrNull { it.screenName == value }
+    infix fun forRoute(value: String?): Screen? = Screen.values().firstOrNull { it.route == value }
   }
 }
